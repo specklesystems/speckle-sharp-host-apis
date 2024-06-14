a8441c421daea9ea82f353374a5dd6788dd7a2c9
Adam Hathcock <adam@speckle.systems>
2024-06-14 12:45:03 +0100

    more rhino!

--- Rhino/Speckle.Rhino.Api/IRhinoCurve.cs
+++ Rhino/Speckle.Rhino.Api/IRhinoCurve.cs
@@ -1,4 +1,6 @@
-﻿using Rhino.Geometry;
+﻿using Rhino.DocObjects;
+using Rhino.Geometry;
+using Rhino.Runtime;
 using Speckle.ProxyGenerator;
 using Speckle.Rhino7.Interfaces;
 
@@ -10,3 +12,37 @@ namespace Speckle.Rhino7.Api;
   new[] { "Duplicate" }
 )]
 public partial interface IRhinoCurveProxy : IRhinoCurve;
+
+[Proxy(
+  typeof(GeometryBase),
+  ImplementationOptions.UseExtendedInterfaces | ImplementationOptions.ProxyForBaseInterface
+)]
+public partial interface IRhinoGeometryBaseProxy : IRhinoGeometryBase;
+
+[Proxy(
+  typeof(CommonObject),
+  ImplementationOptions.UseExtendedInterfaces | ImplementationOptions.ProxyForBaseInterface
+)]
+public partial interface IRhinoCommonObjectProxy : IRhinoCommonObject;
+
+[Proxy(
+  typeof(RhinoObject),
+  ImplementationOptions.UseExtendedInterfaces | ImplementationOptions.ProxyForBaseInterface,
+new[] { "ComponentType" }
+)]
+public partial interface IRhinoObjectProxy : IRhinoObject;
+
+[Proxy(
+  typeof(ModelComponent),
+  ImplementationOptions.UseExtendedInterfaces | ImplementationOptions.ProxyForBaseInterface
+)]
+public partial interface IRhinoModelComponentProxy : IRhinoModelComponent;
+[Proxy(
+  typeof(ArcCurve),
+  ImplementationOptions.UseExtendedInterfaces | ImplementationOptions.ProxyForBaseInterface)]
+public partial interface IRhinoArcCurveProxy : IRhinoArcCurve;
+[Proxy(
+  typeof(Circle),
+  ImplementationOptions.UseExtendedInterfaces | ImplementationOptions.ProxyForBaseInterface
+)]
+public partial interface IRhinoCircleProxy : IRhinoCircle;
--- Rhino/Speckle.Rhino.Interfaces/RhinoCurve.cs
+++ Rhino/Speckle.Rhino.Interfaces/RhinoCurve.cs
@@ -1,6 +1,14 @@
 ﻿namespace Speckle.Rhino7.Interfaces;
 
-public interface IRhinoCurve
-{
-  
-}
+public interface IRhinoCurve : IRhinoGeometryBase;
+
+public interface IRhinoGeometryBase : IRhinoCommonObject;
+
+public interface IRhinoCommonObject;
+
+public interface IRhinoObject : IRhinoModelComponent;
+
+public interface IRhinoModelComponent : IRhinoCommonObject;
+
+public interface IRhinoArcCurve : IRhinoCurve;
+public interface IRhinoCircle;
