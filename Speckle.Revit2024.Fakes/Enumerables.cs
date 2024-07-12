11fa9261f772e8dc2100cf4c8c9acc1a6b7754e3
Adam Hathcock <adam@speckle.systems>
2024-07-12 09:23:56 +0100

    copied fill ins from 2023

---
+++ Speckle.Revit2024.Fakes/Enumerables.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Autodesk.Revit.DB;
+
+public partial class Definitions
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class CurveLoop
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class ExportFontTable
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class ExportPatternTable
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class ExportLineweightTableIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class ExportPatternTableIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class ExportFontTableIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class CurveLoopIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class ExportLayerTableIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class ExportLinetypeTableIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class GeometryElement
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class ExportLinetypeTable
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class ExportLineweightTable
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class ExportLayerTable
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class SolidCurveIntersection
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class KeyBasedTreeEntries
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class FilteredElementCollector
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class FilteredWorksetCollector
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class DefinitionGroups
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class ComponentRepeater
+{
+  IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+}
+
+public partial class ComponentRepeaterIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class KeyBasedTreeEntriesIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class FilteredElementIdIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class FilteredElementIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class FilteredWorksetIdIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
+
+public partial class FilteredWorksetIterator
+{
+  object IEnumerator.Current => throw new System.NotImplementedException();
+}
